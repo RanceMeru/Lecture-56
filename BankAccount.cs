04e30e8ba03054f0e8adc6ef7996138a56910cf2
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-06-21 09:53:04 -0700

    new commits and changes

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,4 +1,39 @@
 class BankAccount
 {
-    
+    private double money;
+    private string owner;
+
+
+    public BankAccount(double money, string owner)
+    {
+        this.money = money;
+        this.owner = owner;
+    }
+    public double Money
+    {
+        get
+        {
+            return money;
+        }
+
+
+    } 
+
+    public string Owner
+    {
+        get
+        {
+            return owner;
+        }
+    }
+
+    public static BankAccount operator + (BankAccount bank1, BankAccount bank2)
+    {
+        int newAmountOfMoney = bank1.Money + bank2.Money;
+        string owner = bank1.Owner + "+" + bank2.Owner;
+
+        BankAccount newBankAccount = new BankAccount(newAmountOfMoney,newOwners);
+        return newBankAccount;
+
+    }
 }
